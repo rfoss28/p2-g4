7a502806973672096013e92716648a0dc8c28614
jrkerr2 <johnrkerr@hotmail.com>
2018-10-03 20:34:59 -0400

    2nd commit for Marc

---
+++ models/user.js
@@ -0,0 +1,27 @@
+module.exports = function(sequelize, DataTypes) {
+    var User = sequelize.define("user", {
+      user_name: DataTypes.STRING,
+      user_email: DataTypes.STRING,
+      user_password: DataTypes.STRING,
+      user_secid: DataTypes.INTEGER,
+      user_delete: DataTypes.BOOLEAN
+
+    });
+
+    
+    User.associate = function(models) {
+      // We're saying that a Post should belong to an Author
+      // A Post can't be created without an Author due to the foreign key constraint
+      User.belongsToMany(models.med, { 
+        as: 'Meds', 
+        through: { model: models.UserMed, unique: false }, 
+        foreignKey: 'user_id',
+        targetKey: 'id'
+        
+      });
+    };
+    return User;
+    
+  };
+
+  
