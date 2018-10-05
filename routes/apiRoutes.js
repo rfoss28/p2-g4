ee7ec368284f1894a604efc388a9509f1f987b68
jrkerr2 <johnrkerr@hotmail.com>
2018-10-05 08:50:08 -0400

    commit for api route queries

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -8,6 +8,20 @@ module.exports = function(app) {
     });
   });
 
+  // get ALL USERS
+  app.get("/api/users", function(req, res) {
+    db.user.findAll({}).then(function(dbuser) {
+      res.json(dbuser);
+    });
+  });
+
+  // get specific USER
+  app.get("/api/users/:id", function(req, res) {
+    db.user.findAll({ where: { id: req.params.id } }).then(function(dbuser) {
+      res.json(dbuser);
+    });
+  });
+
   // Create a new example
   app.post("/api/examples", function(req, res) {
     db.Example.create(req.body).then(function(dbExample) {
@@ -15,10 +29,17 @@ module.exports = function(app) {
     });
   });
 
+  // Create a new USER
+  app.post("/api/users", function(req, res) {
+    db.user.create(req.body).then(function(dbuser) {
+      res.json(dbuser);
+    });
+  });
+
   // Delete an example by id
-  app.delete("/api/examples/:id", function(req, res) {
-    db.Example.destroy({ where: { id: req.params.id } }).then(function(dbExample) {
-      res.json(dbExample);
+  app.delete("/api/users/delete/:id", function(req, res) {
+    db.user.destroy({ where: { id: req.params.id } }).then(function(dbuser) {
+      res.json(dbuser);
     });
   });
 };
