0f0bc8a30961c30e99f1efd17927345b8f631fea
jrkerr2 <johnrkerr@hotmail.com>
2018-10-06 13:25:06 -0400

    merge conflict 2

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,20 +1,10 @@
 var db = require("../models");
 
-<<<<<<< HEAD
-module.exports = function(app) {
   
   // get ALL USERs
-=======
 module.exports = function (app) {
-  // Get all examples
-  app.get("/api/examples", function (req, res) {
-    db.Example.findAll({}).then(function (dbExamples) {
-      res.json(dbExamples);
-    });
-  });
-
+ 
   // get ALL USERS
->>>>>>> master
   app.get("/api/users", function(req, res) {
     db.user.findAll({}).then(function(dbuser) {
       res.json(dbuser);
@@ -28,21 +18,13 @@ module.exports = function (app) {
     });
   });
 
-<<<<<<< HEAD
   // get specific USER (by NAME)
   app.get("/api/names/:name", function(req, res) {
     db.user.findOne({ where: { user_name: req.params.name } }).then(function(dbuser) {
       res.json(dbuser);
-=======
-  // Create a new example
-  app.post("/api/login", function (req, res) {
-    db.Example.create(req.body).then(function (dbExample) {
-      //res.json(dbExample);
-      res.redirect("/index");
->>>>>>> master
     });
   });
-
+  
   // Create a new USER
   app.post("/api/users", function(req, res) {
     db.user.create(req.body).then(function(dbuser) {
@@ -50,11 +32,9 @@ module.exports = function (app) {
     });
   });
 
-  // Delete an example by id
-<<<<<<< HEAD
+  // Delete an example by NAME
   app.delete("/api/delete/:name", function(req, res) {
     db.user.destroy({ where: { user_name: req.params.name } }).then(function(dbuser) {
-=======
 
     });
   });
@@ -64,11 +44,9 @@ module.exports = function (app) {
   app.post("/api/users", function (req, res) {
     db.user.create(req.body).then(function (dbuser) {
       console.log('we hit it');
->>>>>>> master
       res.json(dbuser);
       console.log(dbuser)
     });
   });
-
 
 };
