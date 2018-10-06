958b2ef225f5b2e1a076be0feeda72e2e108c461
jrkerr2 <johnrkerr@hotmail.com>
2018-10-06 13:02:08 -0400

    mvp database commit

--- models/user.js
+++ models/user.js
@@ -3,24 +3,31 @@ module.exports = function(sequelize, DataTypes) {
       user_name: DataTypes.STRING,
       user_email: DataTypes.STRING,
       user_password: DataTypes.STRING,
+
+      user_med1: DataTypes.STRING,
+      user_med2: DataTypes.STRING,
+      user_med3: DataTypes.STRING,
+      user_med4: DataTypes.STRING,
+      user_med5: DataTypes.STRING,
+
       user_secid: DataTypes.INTEGER,
       user_delete: DataTypes.BOOLEAN
 
     });
 
     
-    User.associate = function(models) {
-      // We're saying that a Post should belong to an Author
-      // A Post can't be created without an Author due to the foreign key constraint
-      User.belongsToMany(models.med, { 
-        // as: 'meds', 
-        through: { model: 'UserMed', unique: false }, 
-        // foreignKey: 'user_id',
-        // targetKey: 'id',
-        // timestamps: false
+    // User.associate = function(models) {
+    //   // We're saying that a Post should belong to an Author
+    //   // A Post can't be created without an Author due to the foreign key constraint
+    //   User.belongsToMany(models.med, { 
+    //     // as: 'meds', 
+    //     through: { model: 'UserMed', unique: false }, 
+    //     // foreignKey: 'user_id',
+    //     // targetKey: 'id',
+    //     // timestamps: false
 
-      });
-    };
+    //   });
+    // };
     return User;
     
   };
--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,13 +1,7 @@
 var db = require("../models");
 
 module.exports = function(app) {
-  // Get all examples
-  app.get("/api/examples", function(req, res) {
-    db.Example.findAll({}).then(function(dbExamples) {
-      res.json(dbExamples);
-    });
-  });
-
+  
   // get ALL USERs
   app.get("/api/users", function(req, res) {
     db.user.findAll({}).then(function(dbuser) {
@@ -15,30 +9,17 @@ module.exports = function(app) {
     });
   });
 
-  // get ALL MEDs
-  app.get("/api/meds", function(req, res) {
-    db.med.findAll({}).then(function(dbmed) {
-      res.json(dbmed);
-    });
-  });
-
-//   db.Outlet.findAll({include: [
-//     {model:db.Product, attributes: ['id', 'name', 'nameKh']}
-//     ]}).then(function(outlets){
-//     return res.jsonp(outlets);
-// })
-
-  // get specific USER
+  // get specific USER (by ID)
   app.get("/api/users/:id", function(req, res) {
     db.user.findAll({ where: { id: req.params.id } }).then(function(dbuser) {
       res.json(dbuser);
     });
   });
 
-  // Create a new example
-  app.post("/api/examples", function(req, res) {
-    db.Example.create(req.body).then(function(dbExample) {
-      res.json(dbExample);
+  // get specific USER (by NAME)
+  app.get("/api/names/:name", function(req, res) {
+    db.user.findOne({ where: { user_name: req.params.name } }).then(function(dbuser) {
+      res.json(dbuser);
     });
   });
 
@@ -49,62 +30,9 @@ module.exports = function(app) {
     });
   });
 
-   // Create a new USER with MEDs
-   app.post("/api/usermeds", function(req, res) {
-    console.log(req.body.user_name);
-    console.log(req.body.Meds);
-    db.user.create(
-      req.body
-      ).then(function(user) {
-        console.log('I HAVE USER:', user);
-        return db.med.create(req.body.Meds[1])
-
-      }).then(function(med) {
-        console.log('MED IS:', med);
-        return db.user.addMed(med)
-
-      }).then(function() {
-        res.sendStatus(201);
-      }).catch(function() {
-        res.sendStatus(400);
-      }) 
-    });
-
-/*     db.user.create(req.body,{
-      include: [{
-        as: "meds",
-        model: db.med,
-        //include: [{
-        //  med_id: db.med.med_id,
-        //  med_name: db.med.med_name
-
-        //}]
-
-      }]
-
-    }).then(function(dbuser) {
-      res.json(dbuser);
-    }); */
-
-  
-
-//   models.Survey.create(survey,{
-//     include:  [models.Question,{include: [models.Option]}]
-//   }).then(function() {
-// reply({success:1});
-// });
-
-  // Create a new MED
-  app.post("/api/meds", function(req, res) {
-    console.log(req.body);
-    db.med.bulkCreate(req.body).then(function(dbmed) {
-      res.json(dbmed);
-    });
-  });
-
   // Delete an example by id
-  app.delete("/api/users/delete/:id", function(req, res) {
-    db.user.destroy({ where: { id: req.params.id } }).then(function(dbuser) {
+  app.delete("/api/delete/:name", function(req, res) {
+    db.user.destroy({ where: { user_name: req.params.name } }).then(function(dbuser) {
       res.json(dbuser);
     });
   });
